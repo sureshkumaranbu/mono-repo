f7545a622c1e2f1f356f706cc72ebee326801624
sureshkumaranbu <sureshkumar.anbu@gmail.com>
2022-01-24 18:25:56 -0500

    first commit

---
+++ app1/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "app1": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
